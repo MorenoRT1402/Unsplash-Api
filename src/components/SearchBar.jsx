46d54faeece75ee68c06a72cddc718594dc5f4a9
MorenoRT1402 <morenort1402@gmail.com>
2024-07-29 17:05:45 +0200

    feat: header layout

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,8 @@
 export const SearchBar = () => {
     return (
-        <div className="searchbar"></div>
+        <div className="searchbar">
+            <img src="../../res/images/icon.png" alt="" />
+            <input type="text" placeholder="Busca imágenes en Unsplash" />
+        </div>
     );
 }
