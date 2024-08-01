eef04574be6655e7dbcdb64f6083e4bd6b4f42c6
MorenoRT1402 <morenort1402@gmail.com>
2024-08-01 16:05:02 +0200

    feat: favouriteSlice actions

---
+++ src/features/favourites/favouritesSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    images: []
+}
+
+export const favouritesSlice = createSlice({
+    name: 'favourites',
+    initialState,
+    reducers: {
+        add: (state, action) => {
+            state.images.push(action.payload);
+        },
+        remove: (state, action) => {
+            state.images = state.images.filter(img => img.id != action.payload);
+        },
+        modifyDescription: (state, action) => {
+            const { id, newDescription } = action.payload;
+            const image = state.images.find(img => img.id === id);
+            if (image) {
+                image.description = newDescription;
+            }
+        }
+    }
+})
