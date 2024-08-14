bb8e2e1f8c00edc8035c5584691731b2dbdb0e78
MorenoRT1402 <morenort1402@gmail.com>
2024-08-14 18:24:12 +0200

    feat: footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,14 @@
+import { imagesPath } from "../app/config/paths"
+
 export const Footer = () => {
     return (
-        <footer>
-            linkedin
+        <footer className="footer">
+            <button className="footer__button">
+                <a href="https://www.linkedin.com/in/carlos-moreno-ortega" target="_blank" rel="noopener noreferrer">
+                    <img src={`${imagesPath}/Danleech-Simple-Linkedin.512.png`} alt="LinkedIn" className="footer__icon" />
+                </a>
+            </button>
+            <h5 className="footer__text">© 2024, All rights reserved</h5>
         </footer>
     )
 }
